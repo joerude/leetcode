58c087ba445a32ed4205be1de8d5de5b8a835af3
ma4akk <ma4akk@gmail.com>
2022-01-03 00:59:10 +0300

    files

---
+++ 13.roman_to_integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+            d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+                 'C': 100, 'D': 500, 'M': 1000}
+            c = 0
+
+            for i in range(len(s) - 1):
+                if d[s[i]] < d[s[i+1]]:
+                    # if (d[s[i] == 'I') 
+                    c -= d[s[i]]
+                else: 
+                    c += d[s[i]]
+
+            return c + d[s[-1]]
