3d24ccef5df438950836f9c3e35565804013f7ea
ma4akk <ma4akk@gmail.com>
2022-01-03 00:57:56 +0300

    adding files

---
+++ 20.valid_parentheses :.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for ch in s:
+            if ch in '([{':
+                stack.append(ch)
+    
+            if ch in ')]}':
+                if not stack: return False
+                
+                last_p = stack.pop()
+                if (ch == ')' and last_p != '(') or \
+                    (ch == ']' and last_p != '[') or \
+                    (ch == '}' and last_p != '{'):
+                    return False
+                
+        return False if stack else True  
